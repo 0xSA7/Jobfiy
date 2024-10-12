37d8fd523e62be9460a813894cc9c57367fae826
Mina Khalifa <88984868+Mina-Kalifa@users.noreply.github.com>
2024-10-12 18:38:35 +0300

    Job Saved Handled

--- app/src/main/java/com/sa7/jobfiy/Database/RoomDBHelper.kt
+++ app/src/main/java/com/sa7/jobfiy/Database/RoomDBHelper.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Job::class], version = 1, exportSchema = false)
+@Database(entities = [Job::class, JobSaved::class], version = 2, exportSchema = false)
 abstract class RoomDBHelper : RoomDatabase() {
     abstract val jobDao: JobDao
 
@@ -15,14 +15,11 @@ abstract class RoomDBHelper : RoomDatabase() {
             return INSTANCE ?: synchronized(this) {
                 val instance = Room
                     .databaseBuilder(context, RoomDBHelper::class.java, "job_db")
+                    .fallbackToDestructiveMigration() // Migration strategy for version update
                     .build()
                 INSTANCE = instance
                 instance
-
             }
-
         }
-
     }
-
 }
